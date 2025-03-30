3167f7f0feef69ab429cf7e55b2aa65fd933ac83
Josh Canterbury <joshcanterburyofficial@gmail.com>
2025-03-30 14:03:43 -0500

    1st steps of setting up project

--- src/components/1HomePage.jsx
+++ src/components/1HomePage.jsx
@@ -1,9 +1,24 @@
-function HomePage () {
+import React from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+
+function HomePage() {
     return (
-        <div>
-            <h1>Welcome!</h1>
-        </div>
-    )
+        <Container fluid className="d-flex align-items-center justify-content-center min-vh-100">
+            <Container className="text-center p-4 border rounded shadow-lg bg-light">
+                <Row className="mb-4">
+                    <Col>
+                        <h1 className="fw-bold p-2">Welcome to our store!</h1>
+                        <h4 className="text-muted p-1">Thank you for visiting our store! <br />We have a wide array or products that we hope you enjoy!</h4>
+                        <p className="pt-3">Click below to view our current inventory.</p>
+                        <Button variant="primary" size="lg" href="/productlist">View Products</Button>
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
+    );
 }
 
 export default HomePage;
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
+
+
 
 function ProductList() {
     const [products, setProducts] = useState([]);
@@ -7,13 +16,54 @@ function ProductList() {
     const [error, setError] = useState(null);
     
     
+    useEffect(() => {
+        axios.get('https://fakestoreapi.com/products')
+        .then(response => {
+            setProducts(response.data);
+            setLoading(false)
+        })
+        .catch (error => {
+            setError(`Failed to load products: ${error.message}`);
+            setLoading(false);
+        })
+
+    }, [])
+
+    if(loading) return (
+        <Container className="d-flex align-items-center justify-content-center min-vh-100">
+        <h3>
+          <Spinner
+            animation="border"
+            variant="info"
+            style={{ marginRight: '15px' }}
+            role="status"
+          />
+          Loading Users...
+        </h3>
+      </Container>
+    )
     
+    if(error) return <p>{error}</p>;
     
     
     return (
-        <div>
-            <h1>Products</h1>
-        </div>
+        <Container>
+            <h2>Products</h2>
+            <Row>
+                {products.map(product => (
+                    <Col key={products.id} className="mt-4">
+                        <Card>
+                            <Card.Body>
+                                <Card.Image href={product.image}></Card.Image>
+                                <Card.Title>{product.name}</Card.Title>
+                                <Card.Subtitle>{product.price}</Card.Subtitle>
+                                <Button href={`/productlist`}></Button>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                ))}
+            </Row>
+        </Container>
     )
 }
 
